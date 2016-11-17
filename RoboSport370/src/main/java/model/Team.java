cc628af6fd3487a104d61b161f1fdfed41602396
Dylan <derbstep@gmail.com>
2016-11-17 13:32:54 -0600

    Team Class

---
+++ RoboSport370/src/main/java/model/Team.java
@@ -0,0 +1,44 @@
+package model;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import model.enums.TeamColour;
+
+public class Team {
+    private Robot scout;
+    private Robot sniper;
+    private Robot tank;
+    private Robot lastRobot;
+    private TeamColour colour;
+    private boolean enabled;
+
+    public Team(TeamColour colour, boolean enabled){
+        this.colour = colour;
+        this.enabled = enabled;
+    }
+
+    public Robot getScout() {
+        return scout;
+    }
+
+    public Robot getSniper() {
+        return sniper;
+    }
+
+    public Robot getTank() {
+        return tank;
+    }
+
+    public Robot getNextRobot() {
+        // TODO Team.getNextRobot()
+        throw new NotImplementedException();
+    }
+
+    public TeamColour getColour() {
+        return colour;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
