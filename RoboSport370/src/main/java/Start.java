800c8fde267ba0fe04c0d391f4f65bb6a81edfc5
Evan <snovan@hotmail.com>
2016-11-22 21:31:50 -0600

    UI is now controlled by ViewController

---
+++ RoboSport370/src/main/java/Start.java
@@ -0,0 +1,13 @@
+
+import javafx.application.Application;
+
+import controller.*;
+
+public class Start{
+
+    public static void main(String[] args) {
+        Application.launch(ViewController.class, (java.lang.String[])null);
+    }
+
+
+}
--- RoboSport370/src/main/java/controller/ViewController.java
+++ RoboSport370/src/main/java/controller/ViewController.java
@@ -1,13 +1,35 @@
 package controller;
 
-import javafx.scene.input.MouseEvent;
-import view.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class ViewController{
-    private MainMenu mainMenu;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-    ViewController(){
-        mainMenu = new MainMenu();
+public class ViewController extends Application{
+
+// this class doe not need constructor because it is application and uses start
+//    ViewController(){
+//        mainMenu = new MainMenu();
+//    }
+
+    public void start(Stage view){
+        try {
+            StackPane page = FXMLLoader.load(ViewController.class.getResource("/view/MainMenuView.fxml"));
+            Scene scene = new Scene(page);
+            view.setScene(scene);
+            view.setTitle("Main Menu");
+            view.show();
+        } catch (Exception ex) {
+            Logger.getLogger(ViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void GoToRules(){
+
     }
 
 
