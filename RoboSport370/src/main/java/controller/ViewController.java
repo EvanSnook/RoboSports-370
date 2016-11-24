6ec364fd4a17fbbcfd1ecdfa39dcb49a7dc398f6
Evan <snovan@hotmail.com>
2016-11-24 15:36:00 -0600

    addedmouseClick functions to ViewController

--- RoboSport370/src/main/java/controller/ViewController.java
+++ RoboSport370/src/main/java/controller/ViewController.java
@@ -22,6 +22,37 @@ public class ViewController{
         this.currentScene = mainMenuScene;
     }
 
+    //universal actions
+    public void showMainMenu(/*MouseEvent mouseEvent*/){
+
+    }
+    public void quit(/*MouseEvent mouseEvent*/){
+
+    }
+
+    //CreateGame Actions
+    public void updateRobotThinkTimeValueLabel(/*MouseEvent mouseEvent*/){
+
+    }
+    public void createGame(/*MouseEvent mouseEvent*/){
+
+    }
+
+    //EditRobot Actions
+    public void createNewRobot(/*MouseEvent mouseEvent*/){
+
+    }
+    public void saveRobot(/*MouseEvent mouseEvent*/){
+
+    }
+    public void cancelRobot(/*MouseEvent mouseEvent*/){
+
+    }
+
+    //MainMenu Actions
+    public void showCreateGame(/*MouseEvent mouseEvent*/){
+
+    }
     public void showRules(MouseEvent mouseEvent) {
         if(rulesStage != null) {
             rulesStage.show();
@@ -40,5 +71,33 @@ public class ViewController{
         } catch (Exception ex){
             ex.printStackTrace();
         }
+    }
+    public void showStats(/*MouseEvent mouseEvent*/){
+
+    }
+    public void showRoboManager(/*MouseEvent mouseEvent*/){
+
+    }
+
+    //RoboManager Actions
+    public void editRobot(/*MouseEvent mouseEvent*/){
+
+    }
+    public void importRobot(/*MouseEvent mouseEvent*/){
+
+    }
+    public void exportRobot(/*MouseEvent mouseEvent*/){
+
+    }
+    public void deleteRobot(/*MouseEvent mouseEvent*/){
+
+    }
+
+    //Stats Actions
+    public void uploadStats(/*MouseEvent mouseEvent*/){
+
+    }
+    public void resetStats(/*MouseEvent mouseEvent*/){
+
     }
 }
