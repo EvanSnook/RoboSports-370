5f033f73ac9a090794740acd8a6ca10c7ea385b4
Dylan P <Derbstep@gmail.com>
2016-11-26 15:36:25 -0600

    HexNodeIterator setup. Fixing commit that contained errors.

--- RoboSport370/src/main/java/model/HexNodeIterator.java
+++ RoboSport370/src/main/java/model/HexNodeIterator.java
@@ -4,44 +4,79 @@ import java.util.Iterator;
 
 public class HexNodeIterator implements Iterator{
     private HexNode root;
+    private final int outDirection;
+
     private HexNode currentNode;
+    private int currentFacing;
     private int currentLayer;
     private int currentIndex;
-
-    public HexNodeIterator(HexNode root) {
-        this.root = root;
-        this.currentLayer = 0;
-        this.currentIndex = 0;
-    }
 
     public static void main(String[] args) {
         HexNodeIterator iterator = new HexNodeIterator(new HexNode());
 
         while(iterator.hasNext()){
             HexNode current = iterator.next();
-            System.out.println(current.toDebugString());
+            System.out.println(current.toString());
         }
+    }
+
+    public HexNodeIterator(HexNode root) {
+        this(root, 0);
+    }
+
+    public HexNodeIterator(HexNode root, int startFacing){
+        this.root = root;
+        this.currentNode = root;
+        this.currentLayer = 0;
+        this.currentIndex = 0;
+        this.outDirection = startFacing;
+        this.currentFacing = startFacing + 2;
     }
 
     @Override
     public HexNode next() {
-        // TODO HexNodeIterator.next()
-        return null;
+        if(currentIndex > nodesOnLayer() - 1){
+            out();
+        } else {
+            // TODO get the next ndoe on this layer
+        }
+
+        return currentNode;
     }
 
     @Override
     public boolean hasNext() {
-        if(currentIndex-1 == nodesOnLayer(currentLayer))
+        if(currentIndex <= nodesOnLayer() - 1)
+            return true;
+
+        if(currentIndex > nodesOnLayer() - 1)
             if(currentNode.getR() != null)
                 return true;
+
         return false;
+    }
+
+    /**
+     * get the amount of nodes on this layer
+     * @return the amount of nodes on this layer
+     */
+    private int nodesOnLayer(){
+        if(currentLayer == 0)
+            return 1;
+
+        return currentLayer * 6;
+    }
+
+    /**
+     * Move the cursor one unit away from the starting location.
+     */
+    private void out(){
+        currentLayer++;
+        currentIndex = 0;
+        currentNode = currentNode.get(outDirection);
     }
 
     public HexNode getCurrentNode(){
         return this.currentNode;
-    }
-
-    private int nodesOnLayer(int layer){
-        return layer * 6;
     }
 }
