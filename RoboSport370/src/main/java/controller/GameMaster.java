297f3e547ad65638eca14e6ac3a7e85e741a4e78
Wynston <wcr723@mail.usask.ca>
2016-11-27 16:15:47 -0600

    implemented hexNodeClicked

--- RoboSport370/src/main/java/controller/GameMaster.java
+++ RoboSport370/src/main/java/controller/GameMaster.java
@@ -4,6 +4,7 @@ import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import model.Game;
 import model.HexNode;
+import model.HexNodeIterator;
 import model.Robot;
 
 // TODO GameMaster Class
@@ -19,9 +20,20 @@ public class GameMaster {
     }
 
     public void hexNodeClicked(MouseEvent mouseEvent) {
-        // TODO link the Polygon to the HexNode
-        // set selectedNode
-        System.out.println(((Node) mouseEvent.getSource()).getId());
+        /*
+            Iterators through the board until it finds the selected Polygon that matches the event ID
+            Outputs to stderr if the HexNode could not be found
+         */
+        HexNodeIterator iterator = new HexNodeIterator(game.getBoard().getRoot(),0);
+        while( (iterator.getCurrentNode().getLabel().compareTo(((Node) mouseEvent.getSource()).getId())) != 0){
+            if(iterator.hasNext()) {
+                iterator.next();
+            }
+            else{
+                System.err.println("Can't find selected HexNode.");
+            }
+        }
+        selectedNode = iterator.getCurrentNode();
     }
 
     public Game getGame() {
