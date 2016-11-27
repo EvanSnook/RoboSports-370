d4cf8801ad79190fb18e4747dd8890021cb37830
Dylan P <Derbstep@gmail.com>
2016-11-27 12:38:56 -0600

    Changing between menu views

--- RoboSport370/src/main/java/controller/ViewController.java
+++ RoboSport370/src/main/java/controller/ViewController.java
@@ -1,39 +1,67 @@
 package controller;
 
+import java.io.IOException;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ViewController{
-    private Stage view;
-    private Stage rulesStage;
-    private Scene currentScene;
-    private Scene mainMenuScene;
+    private static Stage view;
+    private static Stage rulesStage;
+    private static Scene currentScene;
 
-    public ViewController() {}
+    private static Scene mainMenuScene;
+    private static Scene createGameScene;
+    private static Scene statsScene;
+    private static Scene robotManagerScene;
+
+    static{
+        try {
+            if(createGameScene == null)
+                createGameScene = new Scene(loadFxml("/view/CreateGameView.fxml"));
+            if(statsScene == null)
+                statsScene = new Scene(loadFxml("/view/RobotStats.fxml"));
+            if(robotManagerScene == null)
+                robotManagerScene = new Scene(loadFxml("/view/RobotManagerView.fxml"));
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public ViewController() {
+    }
 
     public void setStage(Stage stage) {
-        this.view = stage;
-        this.mainMenuScene = stage.getScene();
-        this.currentScene = mainMenuScene;
+        view = stage;
+        mainMenuScene = stage.getScene();
+        currentScene = mainMenuScene;
+    }
+
+    public void setScene(Scene scene){
+        currentScene = scene;
+        view.setScene(scene);
     }
 
     //universal actions
     public void showMainMenu(/*MouseEvent mouseEvent*/){
-
+        setScene(mainMenuScene);
     }
-    public void quit(/*MouseEvent mouseEvent*/){
 
+    public void quit(/*MouseEvent mouseEvent*/){
+        System.exit(0);
     }
 
     //CreateGame Actions
     public void updateRobotThinkTimeValueLabel(/*MouseEvent mouseEvent*/){
 
     }
+
     public void createGame(/*MouseEvent mouseEvent*/){
 
     }
@@ -42,17 +70,20 @@ public class ViewController{
     public void createNewRobot(/*MouseEvent mouseEvent*/){
 
     }
+
     public void saveRobot(/*MouseEvent mouseEvent*/){
 
     }
+
     public void cancelRobot(/*MouseEvent mouseEvent*/){
 
     }
 
     //MainMenu Actions
     public void showCreateGame(/*MouseEvent mouseEvent*/){
-
+        setScene(createGameScene);
     }
+
     public void showRules(MouseEvent mouseEvent) {
         if(rulesStage != null) {
             rulesStage.show();
@@ -60,35 +91,40 @@ public class ViewController{
         }
         try {
             Stage rulesStage = new Stage();
-            Parent rulesMain = FXMLLoader.load(ViewController.class.getResource("/view/RulesView.fxml"));
+            Parent rulesMain = loadFxml("/view/RulesView.fxml");
             rulesStage.setScene(new Scene(rulesMain));
             rulesStage.initModality(Modality.NONE);
 
             rulesStage.initOwner(((Node)mouseEvent.getSource()).getScene().getWindow());
             rulesStage.show();
 
-            this.rulesStage = rulesStage;
-        } catch (Exception ex){
+            rulesStage = rulesStage;
+        } catch (IOException ex){
             ex.printStackTrace();
         }
     }
+
     public void showStats(/*MouseEvent mouseEvent*/){
-
+        setScene(statsScene);
     }
-    public void showRoboManager(/*MouseEvent mouseEvent*/){
 
+    public void showRoboManager(/*MouseEvent mouseEvent*/){
+        setScene(robotManagerScene);
     }
 
     //RoboManager Actions
     public void editRobot(/*MouseEvent mouseEvent*/){
 
     }
+
     public void importRobot(/*MouseEvent mouseEvent*/){
 
     }
+
     public void exportRobot(/*MouseEvent mouseEvent*/){
 
     }
+
     public void deleteRobot(/*MouseEvent mouseEvent*/){
 
     }
@@ -97,7 +133,16 @@ public class ViewController{
     public void uploadStats(/*MouseEvent mouseEvent*/){
 
     }
+
     public void resetStats(/*MouseEvent mouseEvent*/){
 
+    }
+
+    private static Parent loadFxml(String fxmlURL) throws IOException{
+        return FXMLLoader.load(ViewController.class.getResource(fxmlURL));
+    }
+
+    // TODO What is this?
+    public void updateRobotThinkTimeLabel(ScrollEvent scrollEvent) {
     }
 }
