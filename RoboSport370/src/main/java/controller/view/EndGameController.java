a1158048f974b4c1932d2676e6795a46bf7096ed
Evan <snovan@hotmail.com>
2016-12-02 23:25:57 -0600

    end game scene is hooked up to the game as is rules.  main menu is hooked up to endgame

--- RoboSport370/src/main/java/controller/view/EndGameController.java
+++ RoboSport370/src/main/java/controller/view/EndGameController.java
@@ -3,16 +3,17 @@ package controller.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import model.PublicViewController;
 
 public class EndGameController {
     @FXML
     public ListView gameOverviewListView;
 
-    public void quit(/*MouseEvent mouseEvent*/) {
+    public void quit() {
         System.exit(0);
     }
 
-    public void showMainMenu(MouseEvent mouseEvent) {
-
+    public void showMainMenu() {
+        PublicViewController.getInstance().setScene("MAIN_MENU");
     }
 }
